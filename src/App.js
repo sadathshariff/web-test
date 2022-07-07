bce2ce35c73701583d840736140ec7ec6386069b
sadath <sadathshariff65@gmail.com>
2022-07-07 23:10:20 +0530

    Feat :  main layout and  css classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { RouterPath } from "./RouterPath";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterPath />
+    </div>
+  );
 }
 
 export default App;
