1d51c88fa7af4f04eabc27fd1a72943b0f401f2d
sadath <sadathshariff65@gmail.com>
2022-07-07 23:11:13 +0530

    Feat: Main Page

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import "./MainPage.css";
+import { Box, Footer, Header } from "../../components";
+
+export const MainPage = () => {
+  return (
+    <div className="mainpage">
+      <Header />
+      <Box />
+      <Footer />
+    </div>
+  );
+};
